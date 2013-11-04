4be5553e3fc378b502a69c55712b056c6243915b
mrenaud <mrenaud@s19.etu.info-ufr.univ-montp2.fr>
2013-11-04 11:14:46 +0100

    test 4 novembre apres corrections et étude +++

--- todolist/app/controllers/Application.java
+++ todolist/app/controllers/Application.java
@@ -1,38 +1,54 @@
 package controllers;
 
 import play.*;
+import models.*;
 import play.mvc.*;
-
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
 import views.html.*;
-
-package controllers;
-
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import views.html.helper.form;
+import models.Task;
+import play.data.DynamicForm;
+import play.data.Form;
+import play.libs.Json;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class Application extends Controller {
+	static Form<Task> taskForm = Form.form(Task.class);
 	
 		  
 //		  public static Result index() {
 		 //   return ok(index.render("Test du 4 novembre, merci Antoine."));
 		//  }
+	 public static Result index() {
+		  return redirect(routes.Application.tasks());
+		}	
 		  
 		  public static Result tasks() {
-		    return TODO;
+			  return ok(
+					    views.html.index.render(Task.all(), taskForm)
+					  );
 		  }
-		  
 		  public static Result newTask() {
-		    return TODO;
+			  Form<Task> filledForm = taskForm.bindFromRequest();
+			  if(filledForm.hasErrors()) {
+			    return badRequest(
+			      views.html.index.render(Task.all(), filledForm)
+			    );
+			  } else {
+			    Task.create(filledForm.get());
+			    return redirect(routes.Application.tasks());  
+			  }
 		  }
 		  
 		  public static Result deleteTask(Long id) {
 		    return TODO;
 		  }
-		  public static Result index() {
-			  return redirect(routes.Application.tasks());
-			}	  
+		  
+		 
+		 
 		
 
    
--- todolist/app/models/Task.java
+++ todolist/app/models/Task.java
@@ -2,19 +2,38 @@ package models;
 
 import java.util.*;
 
-public class Task {
-    
+import play.data.validation.Constraints.Required;
+import play.mvc.Result;
+import play.db.ebean.*;
+import javax.persistence.*;
+import play.db.ebean.Model;
+import play.db.ebean.Model.Finder;
+
+@Entity
+public class Task extends Model{
+	public static Finder<Long, Task> find = new Finder<Long, Task>(Long.class, Task.class);
+  @Id
   public Long id;
+  @Required
   public String label;
   
   public static List<Task> all() {
-    return new ArrayList<Task>();
+	  return find.all();
+    
   }
   
   public static void create(Task task) {
-  }
+ 
+	  task.save();
+	  }
   
   public static void delete(Long id) {
+	  find.ref(id).delete();
   }
+  
+
+	
+
+	
     
 }
